52923b39645993a754a1a2ab0404978f488d0bad
alamgirjuab <alamgirjuab@gmail.com>
2021-11-13 10:38:50 +0600

    dashboard update

--- src/components/Dashboard/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard/Dashboard.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import AddProduct from '../../AddProduct/AddProduct';
+import MakeAdmin from '../../MakeAdmin/MakeAdmin';
 import './Dashboard.css';
 
 const Dashboard = () => {
     const { user, logout } = useAuth();
+    let { path, url } = useRouteMatch();
     return (
         <div className='row main-body'>
             <div className='col-lg-3 col-sm-3 side-nav'>
                 <h1>this is side bar</h1>
 
-                <NavLink className='link' to="/addProducts">Add Products</NavLink>
-                <NavLink className='link' to="/manageProducts">Manage Products</NavLink>
-                <NavLink className='link' to="/manageOrders">Manage All Orders</NavLink>
-                <NavLink className='link' to="/makeAdmin">Make Admin</NavLink>
-                <NavLink className='link' to="/payment">Payments</NavLink>
-                <NavLink className='link' to="/myOrders">My Orders</NavLink>
-                <NavLink className='link' to="/review">Review</NavLink>
+                <Link className='link' to={`${url}/addProducts`}>Add Products</Link>
+                <Link className='link' to="/manageProducts">Manage Products</Link>
+                <Link className='link' to="/manageOrders">Manage All Orders</Link>
+                <Link className='link' to={`${url}/makeAdmin`}>Make Admin</Link>
+                <Link className='link' to="/payment">Payments</Link>
+                <Link className='link' to="/myOrders">My Orders</Link>
+                <Link className='link' to="/review">Review</Link>
                 <Button className="me-3" onClick={logout} variant="light">Logout</Button>
             </div>
             <div className='col-lg-9 col-sm-9'>
                 <h1>This is content area</h1>
+                <Switch>
+                    <Route exact path={`${path}/addProducts`}>
+                        <AddProduct />
+                    </Route>
+                    <Route path={`${path}/makeAdmin`}>
+                        <MakeAdmin />
+                    </Route>
+                </Switch>
             </div>
         </div>
     );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -17,6 +17,8 @@ const useFirebase = () => {
                 setAuthError('');
                 const newUser = { email, displayName: name };
                 setUser(newUser);
+                // save user to the database
+                saveUser(email, name);
                 // send name ot firebase after creation
                 updateProfile(auth.currentUser, {
                     displayName: name
@@ -69,6 +71,19 @@ const useFirebase = () => {
         })
             .finally(() => setIsloading(false));
     }
+
+    const saveUser = (email, displayName) => {
+        const user = { email, displayName };
+        fetch('http://localhost:5000/users/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then()
+    }
+
     return {
         user,
         isLoading,

499057487523fd1f9a560357405921b6881bf001
alamgirjuab <alamgirjuab@gmail.com>
2022-01-06 22:25:06 +0600

    booking page updated

--- src/components/Booking/Booking/Booking.js
+++ src/components/Booking/Booking/Booking.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
 const Booking = () => {
     const { productId } = useParams();
@@ -11,9 +12,19 @@ const Booking = () => {
     }, [])
     return (
         <div>
-            <h2>Your purchase ID is: {productId}</h2>
-            <h2>Product Name: {product.name}</h2>
+            {/* <h2>Your purchase ID is: {productId}</h2>
+            <h2>Product Name: {product.name}</h2> */}
 
+            <div className="container my-5">
+                <h2 className="text-primary mb-4">Order Your Products</h2>
+                <div className="shadow-lg p-3 mb-5 pb-5 bg-body rounded text-center">
+                    <h2 className="my-4">Booking ID: {productId}</h2>
+                    <img className="rounded mb-4" src={product.img} alt="" />
+                    <h2 className="mb-4">{product.name}</h2>
+                    <p className="w-50 mx-auto mb-4">{product.description}</p>
+                    <NavLink className='btn-success px-3 py-2 rounded-pill mb-4 text-decoration-none' to="/home">Go Home</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
